db0fb024564400ac466562b58b38684513d040f4
William Schramm <william.schramm11@gmail.com>
2021-11-14 13:40:14 -0500

    Small file updates/directory refactoring.

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer (props) {
+    return (
+        <div class="footer" style="position: fixed; left: 0; bottom: 0; width: 100%; background-color: blue; color: white; text-align: center;">
+            <p>Written by Dakota Schramm</p>
+        </div>
+
+    )
+}
