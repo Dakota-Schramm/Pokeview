da6e61f8c4e9a51c292cd85c8368a5c9adc6dcda
William Schramm <william.schramm11@gmail.com>
2021-11-19 17:28:59 -0500

    Added some css to Pokeview page. Added prettier to project.

--- frontend/src/components/search/PokeSearch.js
+++ frontend/src/components/search/PokeSearch.js
@@ -1,8 +1,35 @@
 import {useState, useEffect} from 'react'
 import { Search } from 'react-bootstrap-icons';
 import { Navigate, Outlet } from "react-router-dom";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import Papa from 'papaparse';
+
+import useAutocomplete from './useAutocomplete'
+
+import jirachi from './images/jirachi.png'
+
+
+/*
+    Read from CSV and use to render Pokemon names into the suggestions
+    under the search bar.
+
+    onEnter / on entering search, should redirect to "/input"
+        Need to create state handler to pass value to redirect.
+
+*/
+
+async function fetchCsv (csvFileName) {
+    const response = await fetch(csvFileName);
+    const reader = response.body.getReader();
+    const result = await reader.read();
+    const decoder = new TextDecoder('utf-8');
+    const csv = await decoder.decode(result.value);
+    console.log('csv', csv);
+    return csv;
+}
 
 async function getPokemon () {
     const data = Papa.parse(await fetchCsv('data/pokemon.csv'));
@@ -16,87 +43,49 @@ async function getPokemon () {
     return Pokemon;
 }
 
-async function fetchCsv (csvFileName) {
-    const response = await fetch(csvFileName);
-    const reader = response.body.getReader();
-    const result = await reader.read();
-    const decoder = new TextDecoder('utf-8');
-    const csv = await decoder.decode(result.value);
-    console.log('csv', csv);
-    return csv;
-}
-
-/*
-    Recreate filter function using map to find all names that start with
-    input.
-
-*/
-
-// function filterFunction() {
-//     var input, filter, ul, li, a, i;
-//     input = document.getElementById("myInput");
-//     filter = input.value.toUpperCase();
-//     div = document.getElementById("myDropdown");
-//     a = div.getElementsByTagName("a");
-//     for (i = 0; i < a.length; i++) {
-//         txtValue = a[i].textContent || a[i].innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//             a[i].style.display = "";
-//         } else {
-//             a[i].style.display = "none";
-//         }
-//     }
-// }
-
-/*
-    Read from CSV and use to render Pokemon names into the suggestions
-    under the search bar.
-
-    onEnter / on entering search, should redirect to "/input"
-        Need to create state handler to pass value to redirect.
-
-*/
-
 
 const PokeSearch = (props) => {
-    const [prefix, setPrefix] = useState("")
     const [pokemonInCommon, setPokemonInCommon] = useState([])
+    const { value, handleValueChange, updatePokedex} = useAutocomplete();
 
-    function filterPokemon() {
-        const result = () => {
-            return pokemonInCommon.filter((val) => {
-                return val.startWith(prefix)
-            })
-        }
-        setPokemonInCommon(result)
+    const handler = () => <Navigate to={"/" + value} />
+
+    function handleSubmit (event) {
+        event.preventDefault();
     }
-    const handler = () => <Navigate to={"/" + prefix} />
 
-    // On first render
-    useEffect(() => {
-        setPokemonInCommon(getPokemon())
-    }, []);
-
-    useEffect(() => {
-        filterPokemon()
-    }, [prefix])
+    useEffect(()=> {
+        async function setupPokedex () {
+            const res = await getPokemon()
+            console.log(res)
+            return res 
+        }
+        setupPokedex()
+        .then((res) => {
+            updatePokedex(res)
+        })
+    }, [])
 
     return (
-        <div>
-            <div id="myDropdown" style={{color: '#000000'}}>
-                <Search />
-                <form>
-                    <label>
-                        Pokemon: 
-                    </label>
+        <div className="searchContainer">
+            <h1>POKEVIEW</h1>
+            <img className="jirachi" src={jirachi} alt="A picture of jirachi" />
+            <div style={{color: '#000000'}}>
+                <form 
+                    autoComplete="off"
+                    onSubmit={handleSubmit}
+                >
                     <input type="text" placeholder="Search a pokemon..." 
                         id="myInput"
-                        onKeyDown={e => e.key === 'Enter' && handler()}
-                        value={prefix} onChange={(e)=>setPrefix({value: e.target.value})}
+                        value={value.value}
+                        onChange={handleValueChange}
                     />
+                    <button className="buttonSearch">
+                        <Search />
+                    </button> 
                 </form>
                 {
-                    [].map((pokemon) => {
+                    (value.suggestions)?.map((pokemon) => {
                         return (
                             <a href={`/${pokemon}`}>{pokemon}</a>
                         )
@@ -104,7 +93,6 @@ const PokeSearch = (props) => {
                 }
             </div>
         </div>
-        
     )
 }
 
