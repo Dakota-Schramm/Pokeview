0dc283adf631f2ad76e0fef50f6cbf39282ca25a
William Schramm <william.schramm11@gmail.com>
2021-11-20 23:52:50 -0500

    Added helpers. Created basic card with template from url.

--- frontend/src/components/search/helpers.js
+++ frontend/src/components/search/helpers.js
@@ -1,17 +1,17 @@
-import Papa from 'papaparse';
+import Papa from 'papaparse'
 
-export async function fetchCsv (csvFileName) {
-    const response = await fetch(csvFileName);
-    const reader = response.body.getReader();
-    const result = await reader.read();
-    const decoder = new TextDecoder('utf-8');
-    const csv = await decoder.decode(result.value);
-    console.log('csv', csv);
-    return csv;
+export async function fetchCsv(csvFileName) {
+    const response = await fetch(csvFileName)
+    const reader = response.body.getReader()
+    const result = await reader.read()
+    const decoder = new TextDecoder('utf-8')
+    const csv = await decoder.decode(result.value)
+    console.log('csv', csv)
+    return csv
 }
 
-export async function getPokemon () {
-    const data = Papa.parse(await fetchCsv('data/pokemon.csv'));
+export async function getPokemon() {
+    const data = Papa.parse(await fetchCsv('data/pokemon.csv'))
 
     var Pokemon = []
     for (var entry of data.data) {
@@ -19,5 +19,5 @@ export async function getPokemon () {
     }
     console.log(Pokemon)
 
-    return Pokemon;
+    return Pokemon
 }
---
+++ frontend/src/components/view/Card.js
@@ -0,0 +1,4 @@
+/*
+    Create basic cards modeled off of these templates:
+        https://codepen.io/heatherketten/pen/YaaBde
+*/
