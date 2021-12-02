682275401e467ba750a4853f3807aa89d2dd2df8
William Schramm <william.schramm11@gmail.com>
2021-12-01 22:22:49 -0500

    Reformatted state usage in RegionSlider.

--- frontend/src/components/view/region-slider/RegionSlider.js
+++ frontend/src/components/view/region-slider/RegionSlider.js
@@ -5,7 +5,7 @@ import ToggleButton from 'react-bootstrap/ToggleButton'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 import Placeholder from 'react-bootstrap/Placeholder'
 
-import '../css/RegionSlider.css'
+import './RegionSlider.css'
 
 /* 
     By default, should render the newest region on firstRender.
@@ -15,45 +15,23 @@ import '../css/RegionSlider.css'
 
     TODO
         Implement onLoading action for buttons
+        Need to fix onChange handler.
 */
 
 export default function RegionSlider(props) {
     const generations = props.regions
     const current = props.current
-    console.log(current)
     const setCurrent = props.changeGen
 
-    const [value, setValue] = useState('')
-
-    /*
-     * The second argument that will be passed to
-     * `handleChange` from `ToggleButtonGroup`
-     * is the SyntheticEvent object, but we are
-     * not using it in this example so we will omit it.
-     */
-    const handleChange = (val) => setValue(val)
-
-    /* 
-        Use map to create list elemments for each region.
-        Then, select last one to set as current.
-
-        Not sure if need a tags down below --> 
-            just set onClick to change current
-
-    */
-
-    useEffect(() => {
-        console.log('Value changed', current)
-        setValue(current)
-    }, [current])
-
     function displaySlider() {
-        console.log(generations)
         if (generations !== []) {
             return generations.map((region, idx) => {
                 return (
                     /* Change to onLoading when scrapers loading*/
-                    <ToggleButton id={'tbg-radio-' + idx} value={idx}>
+                    <ToggleButton
+                        id={'tbg-radio-' + idx}
+                        data-testid={'radio' + idx}
+                        value={idx}>
                         {region}
                     </ToggleButton>
                 )
@@ -62,13 +40,13 @@ export default function RegionSlider(props) {
     }
 
     return (
-        <div className="region-slider-buttons">
+        <div className="region-slider-buttons" data-testid="region-slider">
             <ToggleButtonGroup
                 type="radio"
                 name="generations"
-                value={value}
-                onChange={handleChange}
-                defaultValue={value}>
+                value={current}
+                onChange={(val) => setCurrent(val)}
+                defaultValue={current}>
                 {displaySlider()}
             </ToggleButtonGroup>
         </div>
